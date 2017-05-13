3009805d37dae1d71d3d9605a0094938484c7578
Yuval Kohavi <yuval.kohavi@gmail.com>
2017-05-13 16:07:01 -0400

    new files for one wire

---
+++ OneWirePole.cpp
@@ -0,0 +1,30 @@
+#include "OneWirePole.h"
+#include <OneWire.h>
+
+OneWirePole::OneWirePole(int index) : OneWireItem(0x33, index,0,0,0,0,0), scratchpad({0})
+{
+   
+}
+
+
+void OneWirePole::updateCRC()
+{
+    scratchpad[8] = crc8(scratchpad, 8);
+}
+
+void OneWirePole::duty(OneWireHub * const hub)
+{
+    uint8_t cmd;
+    if (hub->recv(&cmd,1)) return;
+
+    switch (cmd)
+    {
+        case CONNECT_READ_STATE: // READ SCRATCHPAD
+            hub->send(scratchpad, 9);
+            break;
+
+        default:
+            hub->raiseSlaveError(cmd);
+    };
+};
+
---
+++ OneWirePole.h
@@ -0,0 +1,30 @@
+#ifndef ONEWIRE_POLE_H
+#define ONEWIRE_POLE_H
+
+#include "utils.h"
+
+#include "OneWireItem.h"
+
+#define CONNECT_READ_STATE 0xBE
+
+class OneWirePole : public OneWireItem
+{
+private:
+
+    uint8_t scratchpad[9];
+
+   void updateCRC();
+
+
+public:
+  inline copy_scrachpad(const uint8_t* data, size_t len) {
+      int sizetocopy = min(len, COUNT_OF(scratchpad)-1);
+      memcpy((void *)scratchpad, data, sizetocopy);
+      updateCRC();
+  }
+
+    OneWirePole(int index);
+    void duty(OneWireHub * const hub);
+};
+
+#endif
